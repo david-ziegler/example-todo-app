1988b15d8af145b732a5867de7763ae84381711f
David Ziegler <david-ziegler@posteo.de>
2025-01-11 14:33:31 +0100

    New: Show todos in table

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import TodoList from "./TodoList";
+import { TodoList } from "./TodoList";
 
 export function App() {
   return (
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,24 +1,55 @@
 import { useQuery } from "@tanstack/react-query";
 import { getTodos } from "../api/todos";
 import { Todo } from "../types/todo";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./shadcn-ui/Table";
+import { LoaderCircleIcon } from "lucide-react";
 
-function TodoList() {
-  const { isPending, error, data } = useQuery<Todo[]>({
+export function TodoList() {
+  const { isPending, data } = useQuery<Todo[]>({
     queryKey: ["todos"],
     queryFn: getTodos,
   });
 
-  if (isPending) {
-    return <div>loading</div>; // TODO
+  if (isPending || !data) {
+    return <LoaderCircleIcon className="animate-spin" />;
   }
+
+  const getDoneLabel = (done: boolean) => {
+    if (done) {
+      return "Erledigt";
+    }
+    return "Offen";
+  };
 
   return (
     <div>
-      {data?.map((todo) => (
-        <div>{todo.id}</div>
-      ))}
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Beschreibung</TableHead>
+            <TableHead>Verantwortlich</TableHead>
+            <TableHead>Fälligkeit</TableHead>
+            <TableHead>Status</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data.map((todo) => (
+            <TableRow key={todo.id}>
+              <TableCell>{todo.label}</TableCell>
+              <TableCell>{todo.responsible}</TableCell>
+              <TableCell>{todo.dueDate}</TableCell>
+              <TableCell>{getDoneLabel(todo.done)}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   );
 }
-
-export default TodoList;
